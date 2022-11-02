dfbfc026bd9dfbb6b47db9cb41ed624cf4418fe5
Robert Sandru <sandrurobert@gmail.com>
2022-11-02 14:45:35 +0200

    Initial Commit

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Robert Sandru on 02.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
